363d1570097a73b11019ec5e7571a6541b5ffaeb
4rty <kingofpunk_edc@hotmail.fr>
2022-09-23 13:19:24 +0200

    Afficher les produits dans le panier 1.2

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,6 +5,7 @@ console.log(cart);
 //---> & appel de la fonction d'affichage des produits
 cart.forEach(element => {
     console.log("IDs :", element.id);
+    console.log("color :", element.color);
     fetch(`http://localhost:3000/api/products/${element.id}`)
         .then(function (res) {
             return res.json();
@@ -17,29 +18,51 @@ cart.forEach(element => {
         });
 });
 
+//---> fonction relative à l'intégration HTML spécifique à chaque produit du panier
 function displayProduct(product) {
-    let article = document.createElement("article");
-    article.setAttribute("class", "cart__item");
-    article.setAttribute("data-id", product._id);
-    article.setAttribute("data-color", product.colors);
-    document.getElementById("cart__items").appendChild(article);
+    let articleProduct = document.createElement("article");
+    articleProduct.setAttribute("class", "cart__item");
+    articleProduct.setAttribute("data-id", product._id);
+    articleProduct.setAttribute("data-color", product.colors);
+    document.getElementById("cart__items").appendChild(articleProduct);
     let div1 = document.createElement("div");
     div1.setAttribute("class", "cart__item__img");
-    document.querySelector(".cart__item").appendChild(div1);
+    articleProduct.appendChild(div1);
     let img = document.createElement("img");
     img.setAttribute("src", product.imageUrl);
     img.setAttribute("alt", product.altTxt);
-    document.querySelector(".cart__item__img").appendChild(img);
+    div1.appendChild(img);
     let div2 = document.createElement("div");
     div2.setAttribute("class", "cart__item__content");
-    document.querySelector(".cart__item").appendChild(div2);
+    articleProduct.appendChild(div2);
     let div2_1 = document.createElement("div");
     div2_1.setAttribute("class", "cart__item__content__description");
-    document.querySelector(".cart__item__content__description").innerHTML += `
+    div2.appendChild(div2_1);
+    div2_1.innerHTML += `
         <h2>${product.name}</h2>
-                    <p>Vert</p>
-                    <p>${product.price} €</p>`;
-    document.querySelector(".cart__item__content").appendChild(div2_1);
-
+        <p>vert à modifier/!</p>
+        <p>${product.price} €</p>`;
+    let div2_2 = document.createElement("div");
+    div2_2.setAttribute("class", "cart__item__content__settings");
+    div2.appendChild(div2_2);
+    let div2_2_1 = document.createElement("div");
+    div2_2_1.setAttribute("class", "cart__item__content__settings__quantity");
+    div2_2.appendChild(div2_2_1);
+    div2_2_1.innerHTML += `
+        <p>Qté : </p>`;
+    let inputQuantity = document.createElement("input");
+    inputQuantity.setAttribute("type", "number");
+    inputQuantity.setAttribute("class", "itemQuantity");
+    inputQuantity.setAttribute("name", "itemQuantity");
+    inputQuantity.setAttribute("min", 1);
+    inputQuantity.setAttribute("max", 100);
+    inputQuantity.setAttribute("value", 42);//>>>>MODIF/!\!!
+    div2_2_1.appendChild(inputQuantity);
+    let div2_2_2 = document.createElement("div");
+    div2_2_2.setAttribute("class", "cart__item__content__settings__delete");
+    div2_2.appendChild(div2_2_2);
+    div2_2_2.innerHTML += `
+        <p class="deleteItem">Supprimer</p>`;
 };
+
 
