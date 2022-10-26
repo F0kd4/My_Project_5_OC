56970f6d53a06f3c33de0b436c73891ddcb23239
4rty <kingofpunk_edc@hotmail.fr>
2022-10-26 15:42:26 +0200

    correction d'un commentaire page "confirmation"

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,4 @@
-
+//---> Récupération du numéro de commande dans l'URL de la page, et affichage de celui-ci par l'HTML
 let params = new URLSearchParams(window.location.search);
 const orderId = params.get("id");
 document.getElementById("orderId").innerHTML += `${orderId}`;
